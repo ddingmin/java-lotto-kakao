44263cf9d814bf9a0633abfe9421c7d37b25d012
bronte000 <bronte000@naver.com>
2024-04-01 17:08:46 +0900

    feat: LottoNumber 클래스 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    public static final int LOWER_BOUND = 1;
+    public static final int UPPER_BOUND = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < LOWER_BOUND || number > UPPER_BOUND) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45까지 가능합니다.");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
