e5eb0f3b2bdfa981e8e5f30341d2b8a4efd138cd
ddingmin <ddingminyt@gmail.com>
2024-04-03 16:29:26 +0900

    refactor: 2뎁스 줄이기

--- src/main/java/lotto/model/dto/LottoResultDto.java
+++ src/main/java/lotto/model/dto/LottoResultDto.java
@@ -3,8 +3,9 @@ package lotto.model.dto;
 import lotto.model.LottoRank;
 import lotto.model.LottoResult;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoResultDto {
     private final long totalPrize;
@@ -14,19 +15,12 @@ public class LottoResultDto {
     public LottoResultDto(LottoResult lottoResult) {
         this.returnRate = lottoResult.calculateReturnRate();
         this.totalPrize = lottoResult.calculateTotalPrize();
-        Map<LottoRank, Integer> lottoResultMap = new HashMap<>();
-        for (LottoRank lottoRank : LottoRank.values()) {
-            if (lottoRank == LottoRank.NONE) {
-                continue;
-            }
-            lottoResultMap.put(lottoRank, 0);
-        }
-        for (LottoRank lottoRank : lottoResult.getLottoRanks()) {
-            if (lottoRank == LottoRank.NONE) {
-                continue;
-            }
-            lottoResultMap.put(lottoRank, lottoResultMap.get(lottoRank) + 1);
-        }
+        Map<LottoRank, Integer> lottoResultMap = Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank != LottoRank.NONE)
+                .collect(Collectors.toMap(lottoRank -> lottoRank, lottoRank -> 0, (a, b) -> b));
+        lottoResult.getLottoRanks().stream()
+                .filter(lottoRank -> lottoRank != LottoRank.NONE)
+                .forEach(lottoRank -> lottoResultMap.put(lottoRank, lottoResultMap.get(lottoRank) + 1));
         this.lottoResult = lottoResultMap;
     }
 
